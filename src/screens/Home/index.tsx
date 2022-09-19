9ece22b121dae061ce9047fdb023fc03a386c56b
Isabella dos Santos Silas <50505852870>
2022-09-19 16:51:21 -0300

    First commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container,Head,Panels,Transactions } from "./style";
+
+export function Home(){
+    return(
+        <Container>
+            <Head>
+
+            </Head>
+            <Panels>
+
+            </Panels>
+            <Transactions>
+
+            </Transactions>
+        </Container>
+    )
+}
---
+++ src/screens/Home/style.tsx
@@ -0,0 +1,22 @@
+// need to install npm install --save @types/styled-components-react-native
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: '#fff';
+`
+
+export const Head = styled.View`
+    flex: 1;
+    background-color: '#fff';
+`
+
+export const Panels = styled.View`
+    flex: 1;
+    background-color: '#fff';
+`
+
+export const Transactions = styled.View`
+    flex: 1;
+    background-color: '#fff';
+`
